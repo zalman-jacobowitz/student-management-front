36421d177ce49b64d7351f7ac7a3d33a4b7ae5d2
Zalman Jacobowitz <zalmanjacobowitz@gmail.com>
2025-07-31 15:43:59 -0400

    add types in students screen

--- src/sections/students/student-new-edit-form.tsx
+++ src/sections/students/student-new-edit-form.tsx
@@ -1,31 +1,26 @@
 import { toast } from "sonner";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { LoadingButton } from "@mui/lab";
 import { Box, Button, DialogActions, DialogContent } from "@mui/material";
 
+import { InfoColumn, InfoStudent } from "src/serverTypes";
 import { infoStudentsUpdate } from "src/actions/info_students";
 
 import { Form } from "src/components/hook-form";
 import { ConfirmDialog } from "src/components/custom-dialog";
-
 import { FromElement } from "src/components/hook-form/dynamic-form/elements";
 
-function coustomizeDisable(columns) {
-  return columns.map(col => {
-    if (col.name === 'student_id' || col.name === 'user_id' || col.name === 'client') {
-      return { ...col, disabled: true };
-    }
-    return col;
-  });
+
+
+
+type StudentsNewEditFromProps = {
+  columns: InfoColumn[]
 }
 
-export function StudentsNewEditFrom({getColumns}){
-  const columns = coustomizeDisable(getColumns || []);
-
-  console.log('columns: ', columns)
+export function StudentsNewEditFrom({ columns }: StudentsNewEditFromProps){
+  
   return (
     <Box
       rowGap={3}
@@ -37,13 +32,25 @@ export function StudentsNewEditFrom({getColumns}){
       {columns.map((col, index) =>
         <Box key={col.name || index} display="flex" flexDirection="column">
           <FromElement info={col} />
-        </Box>)
+        </Box>
+        )
       }
     </Box>
   )
 }
 
-function StudentsNewEditFormDialogContent({getColumns, student, onClose, existingStudents = [] }) {
+
+
+
+type StudentsNewEditFormDialogContentProps = {
+  columns: InfoColumn[],
+  student: InfoStudent,
+  onClose: () => void,
+  existingStudents?: InfoStudent[]
+}
+
+function StudentsNewEditFormDialogContent({columns, student, onClose, existingStudents = [] }: StudentsNewEditFormDialogContentProps) {
+  
   const queryClient = useQueryClient();
 
   const method = useForm({
@@ -54,7 +61,6 @@ function StudentsNewEditFormDialogContent({getColumns, student, onClose, existin
   const {
     handleSubmit,
     formState: { isSubmitting },
-    setError
   } = method;
   
 
@@ -79,36 +85,54 @@ function StudentsNewEditFormDialogContent({getColumns, student, onClose, existin
       toast.error('שגיאה בשליחת הטופס');
     }
   });
+
   
   return (
     <Form methods={method} onSubmit={onSubmit}>
     
       <DialogContent>
-        {/* הצגת שגיאות רק בעת שליחה */}
-
         <Box sx={{ overflow: 'auto' }}>
-          <StudentsNewEditFrom getColumns={getColumns} />
+          <StudentsNewEditFrom columns={columns} />
         </Box>
-
       </DialogContent>
+
       <DialogActions>
         <LoadingButton 
-          type="submit" 
-          variant="contained" 
+          type="submit"
+          variant="contained"
           loading={isSubmitting}
         >
           עדכן
         </LoadingButton>
+        
         <Button variant="outlined" color="inherit" onClick={onClose}>
           ביטול
         </Button>
       </DialogActions>
-      </Form>
+    </Form>
   )
 }
 
+type StudentsNewEditFormDialogProps = {
+  columns: InfoColumn[],
+  student: InfoStudent,
+  open: boolean,
+  onClose: () => void,
+  existingStudents?: InfoStudent[],
+  isEditing: boolean
+}
 
-export function StudentsNewEditFormDialog({getColumns, student, open, onClose, existingStudents = [], isEditing }) {
+export function StudentsNewEditFormDialog(
+  {
+    columns,
+    student,
+    open,
+    onClose,
+    existingStudents = [],
+    isEditing
+  }: StudentsNewEditFormDialogProps
+) {
+
   const title = isEditing ? 'עריכת תלמיד' : 'הוספת תלמיד חדש';
 
   return (
@@ -119,7 +143,7 @@ export function StudentsNewEditFormDialog({getColumns, student, open, onClose, e
       title={title}
       content={
         <StudentsNewEditFormDialogContent 
-          getColumns={getColumns} 
+          columns={columns} 
           student={student} 
           onClose={onClose}
           existingStudents={existingStudents}
