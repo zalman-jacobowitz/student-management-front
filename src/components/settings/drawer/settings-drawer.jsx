1a238ceb29d562bd329c25a16a88c7b858ec919a
Zalman <zalmanjacob@gmail.com>
2025-10-30 15:52:37 -0400

    feat: Update font options and add new font styles for improved typography

--- src/components/settings/drawer/settings-drawer.jsx
+++ src/components/settings/drawer/settings-drawer.jsx
@@ -152,7 +152,7 @@ export function SettingsDrawer({
     <FontOptions
       value={settings.fontFamily}
       onClickOption={(newValue) => settings.onUpdateField('fontFamily', newValue)}
-      options={[defaultFont, 'Inter Variable', 'DM Sans Variable', 'Nunito Sans Variable']}
+      options={[defaultFont, 'Rubik', 'Alef', 'Varela Round']}
     />
   );
 
